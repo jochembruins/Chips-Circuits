6800d764a65c7089435d6a1c1bbef48f6ae5c904
Noah <noah@vgrinsven.com>
2018-04-17 13:33:49 +0200

    Plot grid

--- chips.py
+++ chips.py
@@ -17,6 +17,7 @@
 # pip3 install matplotlib
 
 from numpy import genfromtxt
+import numpy as np
 import matplotlib.pyplot as plt
 
 gatesloc = genfromtxt('gates.csv', delimiter=';')
@@ -32,15 +33,29 @@ class Location(object):
     def __str__(self):
         return "gate: %i, x: %i, y: %i, z: %i" % (self.gate, self.x, self.y, self.z)
 
-gate = []
+gates = []
 for line in gatesloc:
     line = Location(line[0], int(line[1]), int(line[2]), int(line[3]))
-    gate.append(line)
+    gates.append(line)
 
-for obj in gate:
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+# Major ticks every 20, minor ticks every 5
+major_ticks = np.arange(0, 18, 1)
+minor_ticks = np.arange(0, 13, 1)
+
+ax.set_xticks(major_ticks)
+ax.set_xticks(minor_ticks, minor=True)
+ax.set_yticks(major_ticks)
+ax.set_yticks(minor_ticks, minor=True)
+
+# And a corresponding grid
+ax.grid(which='both')
+
+for obj in gates:
     plt.plot(obj.y, obj.x, 'ro')
 plt.axis([0, 17, 12, 0])
-plt.grid()
 plt.show()
 quit()
 
