863da56c3dd97f21578b9e620f2a640086b10db2
Noah <noah@vgrinsven.com>
2018-04-17 12:04:34 +0200

    Dict werkt

--- chips.py
+++ chips.py
@@ -17,25 +17,17 @@
 from numpy import genfromtxt
 gatesloc = genfromtxt('gates.csv', delimiter=';')
 
-print(gatesloc)
-
-plot = []
-for line in gatesloc:
-    plot.append({
-        'gate': int(line[0]),
-        'x': int(line[1]),
-        'y': int(line[2]),
-        'z': int(line[3])})
-
-
-class location(object):
+class Location(object):
     def __init__(self, gate, x, y, z):
-        self.__dict__ = self
         self.gate = gate
         self.x = x
         self.y = y
         self.z = z
 
-grid = []
+plot = []
 for line in gatesloc:
-    grid[location.x,location.y] = 1
+    line = Location(line[0], int(line[1]), int(line[2]), int(line[3]))
+    plot.append(line)
+
+print(plot[2].y)
+
