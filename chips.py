266ae2dbca945095b1886b0c9687478f3deaf0b4
Jochem Bruins <jochembruins@gmail.com>
2018-04-17 11:32:38 +0200

    put gates in dicts

--- chips.py
+++ chips.py
@@ -19,3 +19,15 @@ from numpy import genfromtxt
 gatesloc = genfromtxt('gates.csv', delimiter=';')
 print(gatesloc[5,2])
 
+print(gatesloc)
+
+plot = []
+
+for line in gatesloc:
+	plot.append({
+		'gate' : int(line[0]),
+		'x' : int(line[1]),
+		'y' : int(line[2]),
+		'z' : int(line[3])})
+
+print(plot)
