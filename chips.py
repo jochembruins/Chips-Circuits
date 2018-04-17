5b9f05036bf3337c2329f3617e339287f7ef1cdd
Noah <noah@vgrinsven.com>
2018-04-17 12:55:33 +0200

    Dict final

--- chips.py
+++ chips.py
@@ -20,13 +20,19 @@ gatesloc = genfromtxt('gates.csv', delimiter=';')
 class Location(object):
     def __init__(self, gate, x, y, z):
         self.gate = gate
-        self.x = x
-        self.y = y
-        self.z = z
+        self.x = int(x)
+        self.y = int(y)
+        self.z = int(z)
+
+
+    def __str__(self):
+        return "gate: %i, x: %i, y: %i, z: %i" % (self.gate, self.x, self.y, self.z)
 
 gate = []
 for line in gatesloc:
     line = Location(line[0], int(line[1]), int(line[2]), int(line[3]))
     gate.append(line)
 
+for loc in gate:
+    print(loc)
 
