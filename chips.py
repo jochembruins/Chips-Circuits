ee90c8aeba76d9e1eb6af4e68603b599c3668251
Jochem Bruins <jochembruins@gmail.com>
2018-04-17 15:20:44 +0200

    Working version of chips.py with seperate functions

--- chips.py
+++ chips.py
@@ -35,20 +35,25 @@ class Location(object):
 def main():
     # read gates data
     gatesloc = genfromtxt('gates.csv', delimiter=';')
+    
     gates = makeLocations(gatesloc)
-    printPlot(gates)
+    
     grid = gridMat(gates)
+    
     print(grid)
+    
+    printPlot(gates)
+    
 
 
-def makeLocations(data)
+def makeLocations(data):
     gates = []
     for line in data:
         line = Location(line[0], int(line[1]), int(line[2]), int(line[3]))
         gates.append(line)
     return gates
 
-def printPlot(gates)
+def printPlot(gates):
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
 
@@ -61,18 +66,18 @@ def printPlot(gates)
         plt.plot(obj.x, obj.y, 'ro')    # WAAR KOMEN FIG EN PLOT SAMEN?
         plt.annotate(int(obj.gate), xy=(obj.x, obj.y))
 
-    And a corresponding grid
+    # And a corresponding grid
     plt.grid(which='both')
     plt.axis([0, 17, 12, 0])
     plt.show()
     return
 
-def gridMat(gates)
+def gridMat(gates):
     # make matrix of grid
     matgrid = np.zeros([13,18])
     for gate in gates:
         matgrid[gate.y,gate.x] = gate.gate
-    return matgrid
+    return matgrid 
 
 if __name__ == "__main__":
     main()
