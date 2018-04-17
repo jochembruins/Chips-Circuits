a4b395def8bfe911fe4c9256d194c6589946a567
Noah <noah@vgrinsven.com>
2018-04-17 14:21:33 +0200

    Plot met grid af

--- chips.py
+++ chips.py
@@ -13,6 +13,7 @@
 #
 # Chips & Circuits
 ###########################################################
+# instalrequirements:
 # pip3 install numpy
 # pip3 install matplotlib
 
@@ -20,15 +21,16 @@ from numpy import genfromtxt
 import numpy as np
 import matplotlib.pyplot as plt
 
+# read gates data
 gatesloc = genfromtxt('gates.csv', delimiter=';')
 
+# make objects per gate
 class Location(object):
     def __init__(self, gate, x, y, z):
         self.gate = gate
         self.x = int(x)
         self.y = int(y)
         self.z = int(z)
-
 
     def __str__(self):
         return "gate: %i, x: %i, y: %i, z: %i" % (self.gate, self.x, self.y, self.z)
@@ -41,21 +43,17 @@ for line in gatesloc:
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 
-# Major ticks every 20, minor ticks every 5
+# Define ticks
 major_ticks = np.arange(0, 18, 1)
-minor_ticks = np.arange(0, 13, 1)
-
 ax.set_xticks(major_ticks)
-ax.set_xticks(minor_ticks, minor=True)
 ax.set_yticks(major_ticks)
-ax.set_yticks(minor_ticks, minor=True)
-
-# And a corresponding grid
-ax.grid(which='both')
 
 for obj in gates:
-    plt.plot(obj.x, obj.y, 'ro')
+    plt.plot(obj.x, obj.y, 'ro')    # WAAR KOMEN FIG EN PLOT SAMEN?
     plt.annotate(int(obj.gate), xy=(obj.x, obj.y))
+
+# And a corresponding grid
+plt.grid(which='both')
 plt.axis([0, 17, 12, 0])
 plt.show()
 quit()
