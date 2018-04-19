8c095e3a7fb6e4d626b925d0128e9b44c9134124
Jochem Bruins <jochembruins@gmail.com>
2018-04-19 11:30:07 +0200

    Delete functions in chips.py

--- Scripts/chips.py
+++ Scripts/chips.py
@@ -34,45 +34,7 @@ def main():
     grid = gridMat(gates)
     
     print(grid)
-<<<<<<< HEAD
-    
 
-=======
-
-
-def makeLocations(data):
-    gates = []
-    for line in data:
-        line = Location(line[0], int(line[1]), int(line[2]), int(line[3]))
-        gates.append(line)
-    return gates
-
-def printPlot(gates):
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-
-    # Define ticks
-    ticks = np.arange(0, 18, 1)
-    ax.set_xticks(ticks)
-    ax.set_yticks(ticks)
-
-    for obj in gates:
-        plt.plot(obj.x, obj.y, 'ro')    # WAAR KOMEN FIG EN PLOT SAMEN?
-        plt.annotate(int(obj.gate), xy=(obj.x, obj.y))
-
-    # And a corresponding grid
-    plt.grid(which='both')
-    plt.axis([0, 17, 12, 0])
-    plt.show()
-    return
-
-def gridMat(gates):
-    # make matrix of grid
-    matgrid = np.zeros([13,18])
-    for gate in gates:
-        matgrid[gate.y,gate.x] = gate.gate
-    return matgrid 
->>>>>>> ee8a6d6b6cf2822fb6322244c693407068675703
 
 if __name__ == "__main__":
     main()
