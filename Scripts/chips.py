f9302d3b75ed498137a256001950edc1115e1c58
Melle96 <36193067+Melle96@users.noreply.github.com>
2018-05-15 13:31:51 +0200

    Astar
    q

--- Scripts/chips.py
+++ Scripts/chips.py
@@ -19,8 +19,40 @@ import matplotlib.pyplot as plt
 from netlists import *
 
 # make appropriate format of gate locations
+
 gatesLoc = genfromtxt('../Data/gates.csv', delimiter=';')
 gates = makeLocations(gatesLoc)
+# for gate in gates:
+#     print(gate)
+# quit()
+grid = gridMat(gates)
+grid2 = gridMat2(gates)
+
+
+# netlistDalton = daltonMethod(netlist_1, gates)
+# print(netlistDalton)
+# quit()
+# print("man man man")
+# for x in range(18):
+#     for y in range(13):
+#         for z in range(7):
+#             if grid2[x][y][0] != 99:
+#                 print("x: ", end='')
+#                 print(x, end='')
+#                 print(" y: ", end='')
+#                 print(y, end='')
+#                 print(" z: ", end='')
+#                 print(z, end='')
+#                 print(" grid: ", end='')
+#                 print(grid2[x][y][z])
+#
+# print("man man man")
+wire= [23, 4]
+print("Astar")
+
+hoi = Astar(gates, [3, 23], grid2)
+print("route")
+print(hoi)
 
 # show plot of gates in grid
 # printPlot(gates)
@@ -30,20 +62,20 @@ gates = makeLocations(gatesLoc)
 
 # sort netlist by dalton-method
 netlistDalton = daltonMethod(netlist_1, gates)
-print(netlistDalton)
+# print(netlistDalton)
 # make objects of netlist
 # routeBook = makeObjects(netlistDalton, gates)
 
 
 # connect gates in netlist
-done = 2
+# done = 2
 # while done != 0:
 #     for wire in routeBook:
 #         if wire.route == []:
 #             done = 1
 #         else:
 #             done = 0
-grid = gridMat(gates)
+# grid = gridMat(gates)
     # print(grid, "leeg")
 
 # DIT MOET IN WHILE LOOP
@@ -71,6 +103,4 @@ grid = gridMat(gates)
 # print(grid)
 # print("hoi")
 # print(grid[1][10])
-print(gates)
-hoi = Astar(gates, (42, 3), grid)
-print(hoi)
+
