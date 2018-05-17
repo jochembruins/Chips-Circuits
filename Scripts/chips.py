165b107acd9d14a49644c1a4518a2c708672da7c
Noah <noah@vgrinsven.com>
2018-05-17 10:38:06 +0200

    chips.py opgeschoond + printplot gone

--- Scripts/chips.py
+++ Scripts/chips.py
@@ -20,24 +20,36 @@ from netlists import *
 from classes import *
 from copy import deepcopy
 
+## DATA
 # make appropriate format of gate locations
-
 gatesLoc = genfromtxt('../Data/gates.csv', delimiter=';')
 gates = makeLocations(gatesLoc)
 
+# initialize 13 x 18 x 8 (= L x W x H) grid with gates
 grid = gridMat(gates)
+
+# make appropriate netlist order
+netlistDalton = wire.daltonMethod(netlist_1, gates)
+
+# make object for each netlist item
+routeBook = makeObjects(netlistDalton, gates)
+
+routeBookEmpty = deepcopy(routeBook)
+
+
+## RANDOM ROUTEFINDER
+# connect gates in netlist
+#routeBookRandom = routeFinder(routeBook, grid)[1]
+#score = getScore(routeBookRandom)
+# print(score)
+
+## A-star ALGORITM
+# AstarAll(routeBookempty, gridAstar)
+
 gridAstar = gridMat2(gates)
 print(gridAstar)
 
-netlistDalton = wire.daltonMethod(netlist_1, gates)
-
-
-routeBook = makeObjects(netlistDalton, gates)
-
-routeBookempty = deepcopy(routeBook)
-
-# AstarAll(routeBookempty, gridAstar)
-for route in routeBookempty:
+for route in routeBookEmpty:
     print(route)
 
 hoi = [(23, 4), (1, 2), (15, 21), (3, 5), (7, 13), (3, 23), (23, 8), (22, 13), (15, 17), (20, 10), (15, 8), (13, 18), (19, 2), (22, 11), (10, 4), (11, 24), (3, 15), (2, 20), (3, 4), (20, 19), (16, 9), (19, 5), (3, 2), (15, 5), (6, 14), (7, 9), (9, 13), (22, 16), (10, 7)]
@@ -65,14 +77,9 @@ for i in hoi:
         print("man man man")
 
 
-# show plot of gates in grid
-# printPlot(gates)
-# connect gates in netlist
-#routeBook = routeFinder(routeBook, grid)[1]
-#score = getScore(routeBook)
-# print(score)
-# score = 1000
 
+## HILLCLIMBER
+# score = 1000
 # # laat hilclimber werken
 # HillClimber = hillClimb(routeBookempty, score, gates, 5000)
 
@@ -80,101 +87,12 @@ for i in hoi:
 # for route in routeBookBest:
 #     print(route.route)
 
+# check = checker(routeBookBest)
+
 
 # print(HillClimber[1])
-
-# lol = checker(routeBookBest)
-# print(lol)
 
 # show needed output
-# print(grid)
-# print(totalScore)
-
-# print grid in matrixform
-# grid = gridMat(gates)
-# emptyGrid = deepcopy(grid)
-#
-# # sort netlist by dalton-method
-
-
-# netlistDalton = wire.daltonMethod(netlist_1, gates)
-# print(netlistDalton)
-#
-#
-# # # make objects of netlist
-
-#
-#
-# # connect gates in netlist
-# routeBook = routeFinder(routeBook, grid)[1]
-# score = getScore(routeBook)
-# # print(score)
-#
-#
-# # laat hilclimber werken
-# HillClimber = hillClimb(routeBookempty, score, gates, 1000)
-#
-# routeBookBest = HillClimber[0]
-# for route in routeBookBest:
-# 	print(route)
-#
-# print(HillClimber[1])
-#
-#
-#
-# # for route in routeBook:
-# #     print(route)
-#     # print(grid, "vol")
-#
-#
-# # show needed output
-# # print(grid)
-# # print(totalScore)
+# print(check)
 # plotLines(gates, routeBookBest)
-#
-#
-# # probeersel Melle
-# # print("hoi")
-# # print(grid)
-# # print("hoi")
-# # print(grid[1][10])
-#
-# # print(gates)
-# # hoi = Astar(gates, (42, 3), grid)
-# # print(hoi)
-#
-# netlistDalton = daltonMethod(netlist_1, gates)
-# # print(netlistDalton)
-# # make objects of netlist
-# # routeBook = makeObjects(netlistDalton, gates)
-#
-#
-# # connect gates in netlist
-# # done = 2
-# # while done != 0:
-# #     for wire in routeBook:
-# #         if wire.route == []:
-# #             done = 1
-# #         else:
-# #             done = 0
-# # grid = gridMat(gates)
-#     # print(grid, "leeg")
-
-# netlistDalton = daltonMethod(netlist_1, gates)
-# print(netlistDalton)
-
-# make objects of netlist
-# routeBook = makeObjects(netlistDalton, gates)
-
-
-# connect gates in netlist
-# done = 2
-# while done != 0:
-#     for wire in routeBook:
-#         if wire.route == []:
-#             done = 1
-#         else:
-#             done = 0
-# grid = gridMat(gates)
-    # print(grid, "leeg")
 
