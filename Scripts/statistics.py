2a94f8b750bb951273fa34530077d4f117888676
Jochem Bruins <jochembruins@gmail.com>
2018-05-23 19:07:05 +0200

    add statistics

---
+++ Scripts/statistics.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.axes3d import Axes3D
+import pandas as pd
+
+def plotRandom(data, bins = 20):
+	hist = data['Score'].plot.hist(bins=20)
+	plt.title('Histogram Random Algoritme')
+	plt.xlabel('Score: Totale lengte wires')
+	plt.ylabel('Frequentie')
+	plt.show(hist)
+
+def plotLine(data, name):
+	lines = data.plot.line()
+	plt.title('Ontwikkeling ' + name)
+	plt.xlabel('Iteraties')
+	plt.ylabel('Score: Totale lengte wires')
+	plt.show(lines) 
+	
+
+def plotChip(gates, routeBook):
+    # maak een nieuwe plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # definieer assen
+    ax.set_xlim([0, 18])
+    ax.set_ylim([0, 13])
+    ax.set_zlim([0, 10])
+
+    # zet ticks op de assem
+    ax.set_xticks(np.arange(0, 18, 1))
+    ax.set_yticks(np.arange(0, 13, 1))
+    ax.set_zticks(np.arange(0, 10, 1))
+
+    # voeg labels toe
+    ax.set_xlabel('x-axis')
+    ax.set_ylabel('y-axis')
+    ax.set_zlabel('z-axis')
+
+    # voeg alle gates met labels toe
+    for gate in gates:
+        ax.scatter(gate.x, gate.y, 0)
+        ax.text(gate.x, gate.y, 0, '%s' % (int(gate.gate)), size=10, zorder=1, color='k')
+
+        # leg wires in plot zoals in het routebook
+    for netPoint in routeBook:
+        ax.plot([step[0] for step in netPoint.route], [step[1] for step in netPoint.route], [step[2] for step in netPoint.route])
+
+    plt.show()
