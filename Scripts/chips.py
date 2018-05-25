1ed4f9bdc828848d2ae147cf48a55e48ddddfc08
Noah <noah@vgrinsven.com>
2018-05-25 16:15:24 +0200

    push

--- Scripts/chips.py
+++ Scripts/chips.py
@@ -39,9 +39,7 @@ print("Wat zou je willen zien?\n"
       "5: Vergelijking van 3 verschillende hillclimbers")
 response1 = input("Maak een keuze: ")
 
-if int(response1) > 0 and int(response1) < 6:
-    continue
-else:
+if not int(response1) > 0 and int(response1) < 6:
     print("Dit is geen geldige input")
     exit()
 
@@ -49,18 +47,17 @@ if response1 == '1':
     print("Kies uit netlist 1 - 6")
     response2 = input("Maak een keuze: ")
 
-
-if response2 == 1:
+if response2 == '1':
     netlist = netlists.netlist_1
-elif response2 == 2:
+elif response2 == '2':
     netlist = netlists.netlist_2
-elif response2 == 3:
+elif response2 == '3':
     netlist = netlists.netlist_3
-elif response2 == 4:
+elif response2 == '4':
     netlist = netlists.netlist_4
-elif response2 == 5:
+elif response2 == '5':
     netlist = netlists.netlist_5
-elif response2 == 6:
+elif response2 == '6':
     netlist = netlists.netlist_6
 else:
     print("Gebruik niet correct")
@@ -68,8 +65,9 @@ else:
           "waar '1' staat voor netlist 1")
     exit()
 
+print(netlist)
 # gebruik kleine of grote grid
-if commArg < 4:
+if int(response2) < 4:
     size = "small"
     # laadt gates locaties voor kleine grid
     gatesLoc = genfromtxt('../Data/gates.csv', delimiter=';')
@@ -94,7 +92,7 @@ routeBookEmpty = deepcopy(routeBook)
 # bepaal lowerbound aka Manhattan distance van netlist
 # DIT MOET IN DE OUTPUTTABEL ERGENS NEERGEZET WORDEN
 lowerBound, netlistDist = functions.manhattanDist(routeBook)
-print("Lowerbound score voor netlist", commArg, ":", lowerBound)
+print("Lowerbound score voor netlist", response2, ":", lowerBound)
 
 
 # ## VERGELIJK VERSCHILLENDE NETLISTS ---------------------------------------
